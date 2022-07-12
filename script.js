fd39c347df4500c738103ff55f2b9860242f2d9d
Asante-Nsiah <88829934+Asante-Nsiah@users.noreply.github.com>
2022-07-12 16:50:33 +0000

    updated files

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert('amali')
+
+
+$(document).ready(function() {
+    $.getJSON("data.json", function(data) { 
+    $.each(data, function(key, value) {
+        value.Title
+
+    });
+    });
+});
