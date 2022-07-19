6f188d3f49aa2a1708b03289f707c86a4f584a20
Asante-Nsiah <88829934+Asante-Nsiah@users.noreply.github.com>
2022-07-19 17:35:14 +0000

    updated mobile view and JS

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
+const daily = document.getElementById("daily-report");
+const weekly = document.getElementById("weekly-report");
+const monthly = document.getElementById("monthly-report");
+const titleRob = document.getElementById("title");
+const hoursRob = document.getElementById("hours");
+const prevHour = document.getElementById("past-hour");
 
 
 $(document).ready(function() {
     $.getJSON("data.json", function(data) { 
     $.each(data, function(key, value) {
-        value.Title
+        console.log(value);
+
+        daily.addEventListener("click",  () => {
+            console.log(daily);
+            titleRob.innerText = value.title;
+            hoursRob.innerText = value.timeframes.daily.current+"hrs";
+            document.body.style.color = "white";
+            prevHour.innerText = value.timeframes.daily.previous+"hrs";
+        })
+
+        weekly.addEventListener("click",  () => {
+            console.log(weekly);
+            titleRob.innerText = value.title;
+            hoursRob.innerText = value.timeframes.weekly.current+"hrs";
+            prevHour.innerText = value.timeframes.weekly.previous+"hrs";
+        })
+
+        monthly.addEventListener("click",  () => {
+            console.log(monthly);
+            titleRob.innerText = value.title;
+            hoursRob.innerText = value.timeframes.monthly.current+"hrs";
+            prevHour.innerText = value.timeframes.monthly.previous+"hrs";
+        })
+
+    
+
+
+
+        
 
     });
     });
