c29d429e3759a40a6609518ece20806c0ec31d69
Asante-Nsiah <88829934+Asante-Nsiah@users.noreply.github.com>
2022-07-20 13:27:09 +0000

    Each and switch case in Javascript file

--- script.js
+++ script.js
@@ -1,45 +1,51 @@
 const daily = document.getElementById("daily-report");
 const weekly = document.getElementById("weekly-report");
 const monthly = document.getElementById("monthly-report");
-const titleRob = document.querySelector("#title");
-const hoursRob = document.querySelector("#hours");
-const prevHour = document.querySelector("#past-hour");
+const titleRob = document.querySelector(".title");
+const hoursRob = document.querySelector(".hours");
+const prevHour = document.querySelector(".past-hour");
 
 
-let menu_container = document.createElement("div");
-menu_container.className = "menu-details";
-document.body.appendChild(menu_container);
+
 
 
 $(document).ready(function() {
+
+    const updateDaily = (data,period) => {
+
+        $(".item-container").each(function(index){
+            $(this).find(".title").text(data[index].title);
+
+            switch (period) {
+                case "d":
+                    $(this).find(".hours").text(data[index].timeframes.daily.current+"hrs");
+                    $(this).find(".past-hour").text(data[index].timeframes.daily.previous+"hrs"); 
+                    document.body.style.color = "white";
+                    break;
+
+                    case "w":
+                    $(this).find(".hours").text(data[index].timeframes.weekly.current+"hrs");
+                    $(this).find(".past-hour").text(data[index].timeframes.weekly.previous+"hrs"); 
+                    document.body.style.color = "white";
+                    break;
+                    
+                    case "m":
+                    $(this).find(".hours").text(data[index].timeframes.monthly.current+"hrs");
+                    $(this).find(".past-hour").text(data[index].timeframes.monthly.previous+"hrs"); 
+                    document.body.style.color = "white";
+                    break;
+            
+                default:
+                    break;
+            }
+        })
+    }
     $.getJSON("data.json", function(data){ 
-    $.each(data, function(key, value){
-        console.log(value);
-       
-      
-        daily.addEventListener("click",  () => {
-            console.log(daily);
-            titleRob.innerText = value.title;
-            hoursRob.innerText = value.timeframes.daily.current+"hrs";
-            document.body.style.color = "white";
-            prevHour.innerText = value.timeframes.daily.previous+"hrs";
-        });
+        console.log(data);
+        daily.addEventListener('click', () => updateDaily(data,"d"))
+        weekly.addEventListener('click', () => updateDaily(data,"w"))
+        monthly.addEventListener('click', () => updateDaily(data,"m"))
 
-        weekly.addEventListener("click",  () => {
-            console.log(weekly);
-            titleRob.innerText = value.title;
-            hoursRob.innerText = value.timeframes.weekly.current+"hrs";
-            document.body.style.color = "white";
-            prevHour.innerText = value.timeframes.weekly.previous+"hrs";
-        });
-
-        monthly.addEventListener("click",  () => {
-            console.log(monthly);
-            titleRob.innerText = value.title;
-            hoursRob.innerText = value.timeframes.monthly.current+"hrs";
-            document.body.style.color = "white";
-            prevHour.innerText = value.timeframes.monthly.previous+"hrs";
-        });
-    });
     });
 });
+
