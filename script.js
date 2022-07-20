5ac4d4289db1197bd40fedcb1411edcd2bae5bc7
Asante-Nsiah <88829934+Asante-Nsiah@users.noreply.github.com>
2022-07-20 00:08:27 +0000

    Updated JS

--- script.js
+++ script.js
@@ -1,44 +1,45 @@
 const daily = document.getElementById("daily-report");
 const weekly = document.getElementById("weekly-report");
 const monthly = document.getElementById("monthly-report");
-const titleRob = document.getElementById("title");
-const hoursRob = document.getElementById("hours");
-const prevHour = document.getElementById("past-hour");
+const titleRob = document.querySelector("#title");
+const hoursRob = document.querySelector("#hours");
+const prevHour = document.querySelector("#past-hour");
+
+
+let menu_container = document.createElement("div");
+menu_container.className = "menu-details";
+document.body.appendChild(menu_container);
 
 
 $(document).ready(function() {
-    $.getJSON("data.json", function(data) { 
-    $.each(data, function(key, value) {
+    $.getJSON("data.json", function(data){ 
+    $.each(data, function(key, value){
         console.log(value);
-
+       
+      
         daily.addEventListener("click",  () => {
             console.log(daily);
             titleRob.innerText = value.title;
             hoursRob.innerText = value.timeframes.daily.current+"hrs";
             document.body.style.color = "white";
             prevHour.innerText = value.timeframes.daily.previous+"hrs";
-        })
+        });
 
         weekly.addEventListener("click",  () => {
             console.log(weekly);
             titleRob.innerText = value.title;
             hoursRob.innerText = value.timeframes.weekly.current+"hrs";
+            document.body.style.color = "white";
             prevHour.innerText = value.timeframes.weekly.previous+"hrs";
-        })
+        });
 
         monthly.addEventListener("click",  () => {
             console.log(monthly);
             titleRob.innerText = value.title;
             hoursRob.innerText = value.timeframes.monthly.current+"hrs";
+            document.body.style.color = "white";
             prevHour.innerText = value.timeframes.monthly.previous+"hrs";
-        })
-
-    
-
-
-
-        
-
+        });
     });
     });
 });
